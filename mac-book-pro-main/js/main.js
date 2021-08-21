7eb6104dc4d33e3d633349e63a93a245ee3d6f1f
SyedShajib <syedshajib55@gmail.com>
2021-08-21 19:31:10 +0600

    js

---
+++ mac-book-pro-main/js/main.js
@@ -0,0 +1,92 @@
+//Memory Cost
+function updateMemoryCost(memory){
+	if(memory == 16){
+		document.getElementById('extra-memory').innerText = 180;
+	}
+	else{
+		document.getElementById('extra-memory').innerText = 0;
+	}
+	updateTotal();
+}
+
+//Storage Cost
+function updateStorageCost(storage){
+	if(storage == 256){
+		document.getElementById('extra-storage').innerText = 0;
+	}
+	else if(storage == 512){
+		document.getElementById('extra-storage').innerText = 100;
+	}
+	else{
+		document.getElementById('extra-storage').innerText = 180;
+	}
+	updateTotal();
+}
+
+//Delivery Cost
+function updateDeliveryCost(deliveryType){
+	if(deliveryType == 'normal'){
+		document.getElementById('delivery-cost').innerText = 0;
+	}
+	else {
+		document.getElementById('delivery-cost').innerText = 20;
+	}
+	updateTotal();
+}
+
+//Update Total
+function updateTotal(){
+	const basePrice = 1299;
+	let memoryCost = parseInt (document.getElementById('extra-memory').innerText );
+	let storageCost = parseInt(document.getElementById('extra-storage').innerText);
+	let deliveryCost = parseInt(document.getElementById('delivery-cost').innerText);
+
+	const totalPrice = memoryCost + storageCost + deliveryCost + basePrice ;
+
+	document.getElementById('total-cost').innerText = totalPrice;
+	document.getElementById('grand-total').innerText = totalPrice;
+}
+
+//Update Grand Total
+function applyCoupon(){
+	const totalPrice = parseInt(document.getElementById('total-cost').innerText);
+
+    // Getting input value
+    const promoText = document.getElementById('promo-field').value;
+
+    // Clearing input field
+    document.getElementById('promo-field').value = "";
+
+    // promo code check
+    if (promoText == "stevekaku") {
+        const priceAfterPromo = totalPrice - totalPrice / 5;
+        document.getElementById('grand-total').innerText = parseFloat(priceAfterPromo);
+    }
+}
+
+// Memory Button 
+document.getElementById('memory-8gb').addEventListener('click',function(){
+	updateMemoryCost(8);
+});
+document.getElementById('memory-16gb').addEventListener('click',function(){
+	updateMemoryCost(16);
+});
+
+// Storage Button 
+document.getElementById('storage-256gb').addEventListener('click',function(){
+	updateStorageCost(256);
+});
+document.getElementById('storage-512gb').addEventListener('click',function(){
+	updateStorageCost(512);
+});
+document.getElementById('storage-1TB').addEventListener('click',function(){
+	updateStorageCost(1);
+});
+
+//Delivery Button
+document.getElementById('normal-delivery').addEventListener('click',function(){
+	updateDeliveryCost('normal');
+});
+document.getElementById('early-delivery').addEventListener('click',function(){
+	updateDeliveryCost('early');
+});
